0339a2f9603673550a37c96fe27ba4c2ccc07b8a
blindjedi <matt6js@gmail.com>
2024-12-18 22:12:37 -0700

    enable admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, SocialProfile, Membership, SubscriptionPlan, Subscription, Post
 
-# Register your models here.
+# Register your models here
+admin.site.register(Profile)
+admin.site.register(SocialProfile)
+admin.site.register(Membership)
+admin.site.register(SubscriptionPlan)
+admin.site.register(Subscription)
+admin.site.register(Post)
